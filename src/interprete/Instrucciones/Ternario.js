a1cb914abd8ba400bb34ce6b33b4554c350a883d
Abraham Solórzano <abraham81423@gmail.com>
2021-12-12 20:51:15 -0600

    [FIX] Ternario.
    Retorno del tipo.

--- src/interprete/Instrucciones/Ternario.js
+++ src/interprete/Instrucciones/Ternario.js
@@ -5,20 +5,25 @@ class Ternario extends Instruction{
         this.condition = condition;
         this.instr_true = instr_true;
         this.instr_false = instr_false;
+        this.type = null;
     }
 
     interpretar(tree, table){
         var condition = this.condition.interpretar(tree, table);
-
+        console.log(this.condition.type);
+        console.log(condition)
         if(this.condition.type == Tipo.BOOLEANO){
             
             if(Boolean(condition) == true){ 
                 var value = this.instr_true.interpretar(tree, table);
                 if(value instanceof Exception) return value;
+                this.type = this.instr_true.type;
                 return value;
             }else{
+                console.log('entro')
                 var value = this.instr_false.interpretar(tree, table);
                 if(value instanceof Exception) return value;
+                this.type = this.instr_false.type;
                 return value;
             }
 
