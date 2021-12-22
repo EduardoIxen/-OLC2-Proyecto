dfd871e33abcbaf770bafb318822f1a276d709a5
Abraham Solórzano <abraham81423@gmail.com>
2021-12-22 13:47:27 -0600

    [UPDATE] C3D.

--- src/interprete/TS/C3D.js
+++ src/interprete/TS/C3D.js
@@ -2,7 +2,7 @@ class C3D{
     constructor(value, type, isTemp){
         this.value = value;  // temp
         this.type = type;
-        this.isTemp = isTemp; // temp || label
+        this.isTemp = isTemp; // temp==true || label == false
         this.EV = ''; // ETIQUETA VERDADERA
         this.EF = ''; // ETIQUETA FALSA
         this.posGlobal = null; // POSICIÓN DEL STACK
